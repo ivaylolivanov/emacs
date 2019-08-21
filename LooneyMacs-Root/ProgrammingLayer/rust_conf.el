911492456c3ede3f6c06802a64c270dbde219358
Ivaylo <m00ns7ruck@abv.bg>
2019-08-21 22:39:57 +0300

    rust_conf.el: Rework configurations to use 'use-package'

--- LooneyMacs-Root/ProgrammingLayer/rust_conf.el
+++ LooneyMacs-Root/ProgrammingLayer/rust_conf.el
@@ -5,22 +5,33 @@
 ;; - Activate racer when rust mode starts
 
 ;;; Code:
-;; - Configure rust-mode to auto format the code on save
-(setq rust-format-on-save t)
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
 
-;; - Flycheck-rust
-(with-eval-after-load 'rust-mode
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-(add-hook 'rust-mode-hook #'flycheck-rust-setup)
 
-;; - Activate racer when rust mode starts
-(require 'rust-mode)
-(add-hook 'racer-mode-hook #'company-mode)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-(setq company-tooltip-align-annotations t)
 
+(use-package rust-mode
+  :ensure t
+  :config
+  ;; Flycheck-rust
+  (use-package flycheck-rust
+    :ensure t
+    :config
+    (with-eval-after-load 'rust-mode
+      (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+    (add-hook 'rust-mode-hook #'flycheck-rust-setup))
+
+  ;; - Configure rust-mode to auto format the code on save
+  (setq rust-format-on-save t)
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (setq company-tooltip-align-annotations t))
+
+(use-package racer
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+
+  ;; Activate racer when rust mode starts
+  (add-hook 'racer-mode-hook #'company-mode))
 
 (provide 'rust_conf)
 ;;; rust_conf.el ends here
