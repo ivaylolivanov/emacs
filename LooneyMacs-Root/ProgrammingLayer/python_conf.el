0417f6be5f601601335e2f2e65ece42f96dd5589
Ivaylo <m00ns7ruck@abv.bg>
2019-08-21 22:39:22 +0300

    python_conf.el: Rework configurations to use 'use-package'

--- LooneyMacs-Root/ProgrammingLayer/python_conf.el
+++ LooneyMacs-Root/ProgrammingLayer/python_conf.el
@@ -4,25 +4,28 @@
 ;;; Commentary:
 ;; - This file will house the python configurations
 ;; - Elpy
-;; - Use flycheck with Elpy
 ;; - Py-autopep8
 
 
 
 ;;; Code:
 ;; - Enable Elpy
-(elpy-enable)
-
-
-;; - Flycheck with elpy
-(when (require 'flycheck nil t)
+(use-package elpy
+  :ensure t
+  :init
+  (add-hook 'python-mode-hook #'elpy-enable)
+  :config
+  ;; Flycheck with elpy
+  (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (add-hook 'elpy-mode-hook 'flycheck-mode)))
+
 
 
 ;; - Enable py-autopep8
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(use-package py-autopep8
+  :ensure t
+  :init (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
 
 (provide 'python_conf)
 ;;; python_conf.el ends here
