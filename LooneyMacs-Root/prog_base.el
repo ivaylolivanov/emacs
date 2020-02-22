adeef585b894861c98de4ce4415448a6ff432806
m00ns7ruck <m00ns7ruck@abv.bg>
2020-02-23 01:02:30 +0200

    company: Adjust delay and min prefix length

--- LooneyMacs-Root/prog_base.el
+++ LooneyMacs-Root/prog_base.el
@@ -153,10 +153,10 @@
 (use-package company
   :ensure t
   :config
-  (setq company-idle-delay 0.5)
+  (setq company-idle-delay 0.0)
   (setq company-show-numbers t)
   (setq company-tooltip-limit 10)
-  (setq company-minimum-prefix-length 2)
+  (setq company-minimum-prefix-length 1)
   (setq company-tooltip-align-annotations t)
   (setq company-tooltip-flip-when-above t)
   (global-company-mode))
