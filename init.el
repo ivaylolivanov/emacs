f3dc2b1b42a60e34ca081764aaf4f62f925e5e30
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-10-29 19:40:42 +0200

    init.el: Update minimal supported version
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- init.el
+++ init.el
@@ -19,8 +19,8 @@
    (if (equal system-type 'windows-nt) "USERNAME" "USER")))
 (message "Hello, %s sama, glad you are here again!\nDo your best :)" master)
 
-(when (version< emacs-version "24.4")
-  (error "Please upgrade me, %s sama, my version is: %s and it should be at least: 24.4" master emacs-version))
+(when (version< emacs-version "26.1")
+  (error "Please upgrade me, %s sama, my version is: %s and it should be at least: 26.1" master emacs-version))
 ;;=================================
 
 
