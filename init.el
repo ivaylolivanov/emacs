6c58d877d1625c8a975c0f434d31121e81aa2847
ivaylolivanov <ivaylolivanov95@gmail.com>
2021-08-22 19:03:27 +0300

    init.el: Remove obsolete custom setting
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- init.el
+++ init.el
@@ -124,7 +124,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-project-whitelist (quote ("^/home/ivo/Programming/Projects/IT_Project/$")))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
