643ea0e15fb0e3b7cf2db5dfd7096f046b58c82d
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-11-02 21:55:26 +0200

    Smoother scrolling
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- LooneyMacs-Root/core.el
+++ LooneyMacs-Root/core.el
@@ -11,6 +11,7 @@
 ;;     - Set theme
 ;;     - Enable winner-mode
 ;;     - Dashboard
+;; - Smooth mouse scrolling
 ;; - Dired, Recentf, Ediff and Eldoc
 ;; - Go-to-address mode
 ;; - Warn by oppening large files
@@ -139,6 +140,15 @@
   (dashboard-setup-startup-hook))
 ;;==============================
 
+
+
+;;============================
+;;= Smoother mouse scrolling =
+;;============================
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
 
 
 ;;==============================================
