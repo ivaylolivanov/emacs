265d85e28c917d92905369d3dc5a4b110d13cf12
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-11-07 00:03:02 +0200

    rust-conf: Rework
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- looneymacs-root/programming-layer/rust-conf.el
+++ looneymacs-root/programming-layer/rust-conf.el
@@ -1,42 +1,22 @@
 ;;; rust-conf --- This file houses the rust configurations
 
 ;;; Commentary:
-;; - Enable code formatting on save
-;; - Activate racer when rust mode starts
+;; - Rust-mode configuration
 
 ;;; Code:
-;;=======================
-;;= Rust Configurations =
-;;=======================
-
-
+;;============================
+;;= Rust-mode configurations =
+;;============================
 
 (use-package rust-mode
   :ensure t
   :config
-  ;; Flycheck-rust
-  (use-package flycheck-rust
-    :ensure t
-    :config
-    (with-eval-after-load 'rust-mode
-      (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-    (add-hook 'rust-mode-hook #'flycheck-rust-setup))
-
+  :hook
   ;; - Configure rust-mode to auto format the code on save
   (setq rust-format-on-save t)
-  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
   (setq company-tooltip-align-annotations t))
 
-(use-package racer
-  :ensure t
-  :requires rust-mode
-  :init
-  :config
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-
-  ;; Activate racer when rust mode starts
-  (add-hook 'racer-mode-hook #'company-mode))
+;;============================
 
 (provide 'rust-conf)
 ;;; rust-conf.el ends here
