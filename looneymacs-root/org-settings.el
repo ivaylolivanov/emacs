9e7c9ae5ff8d012dc075af4ead8f268d83101071
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2024-02-28 00:20:32 +0200

    org-settings.el: Add & consfigure org-mode
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- looneymacs-root/org-settings.el
+++ looneymacs-root/org-settings.el
@@ -9,6 +9,7 @@
 ;; - Specify agenda directories
 ;; - Enable visual-line-mode within org-mode
 ;; - Configure org capture
+;; - Org roam mode
 ;; - Keybindings
 
 
@@ -30,6 +31,25 @@
 
 ;; - Configure org capture mode
 (setq org-default-notes-file (concat org-notes-dir "/quick_notes.org"))
+
+;; - Org roam mode
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :after org
+  :custom
+  (org-roam-directory (file-truename org-roam-storage-dir))
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n f" . org-roam-node-find)
+         (:map org-mode-map
+               (("C-c n i" . org-roam-node-insert)
+                ("C-c n o" . org-id-get-create)
+                ("C-c n t" . org-roam-tag-add)
+                ("C-c n a" . org-roam-alias-add)
+                ("C-c n l" . org-roam-buffer-toggle))))
+  :config
+  (org-roam-setup))
 
 ;; - Keybindings
 (global-set-key (kbd "C-c l") 'org-store-link)
