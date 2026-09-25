de25863b647a66e94ef9ecad68e560ddd9001543
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-09-25 13:59:11 +0300

    Update emacs packages
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- init.el
+++ init.el
@@ -136,7 +136,13 @@
  '(package-archives
    '(("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "http://melpa.org/packages/")))
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(company counsel dashboard editorconfig eglot erc faceup flycheck
+             flymake haki-theme htmlize idlwave magit markdown-mode
+             org-roam peg projectile python rainbow-delimiters
+             rust-mode spacemacs-theme sqlite3 tramp use-package
+             verilog-mode volatile-highlights which-key
+             window-tool-bar yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
